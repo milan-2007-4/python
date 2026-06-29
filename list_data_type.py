3045db9b87469c35f8cbc0763e6c3f8063bef006
milan ghodasara <milanghodasara73@gmail.com>
2026-06-29 10:04:50 +0530

    Update list_data_type.py

--- list_data_type.py
+++ list_data_type.py
@@ -7,7 +7,26 @@ for i in a:
 a.append('mengo')
 for i in a:
     print(i)
-x=a.count(2)
-a.remove(4)
+print(a.count(2))
+a.remove(a[5])
 print(a)
-
+a.insert(2,32)
+print(a)
+s=[1,5,4,2,3,6,7]
+s.sort()
+print(s)
+ind=a.index(4)
+print(ind)
+total=a.count(2)
+print(total)
+s.reverse()
+print(s)
+b=a.copy()
+print(b)
+del a
+#print(a)
+n=[['ads',"abc",8+7j],[1,2,3,'mengo']]
+print(n)
+print(n[1])
+print(n[0][2])
+print(n[1][2])
